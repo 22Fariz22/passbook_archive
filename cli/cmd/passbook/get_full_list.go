4a52783c78b1b839f2c9168b7e6b1cbf166e9be3
22fariz22 <fariz08@gmail.com>
2023-05-17 12:21:18 +0300

    add getFullList method

---
+++ cli/cmd/passbook/get_full_list.go
@@ -0,0 +1,41 @@
+package passbook
+
+import (
+	"fmt"
+	"github.com/22Fariz22/passbook/cli/pkg"
+	pb "github.com/22Fariz22/passbook/server/proto"
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+)
+
+var getFullListCmd = &cobra.Command{
+	Use:   "full",
+	Short: "get all your secrets",
+	Long:  "",
+	Run: func(cmd *cobra.Command, args []string) {
+		conn, err := grpc.Dial(":5001", grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer conn.Close()
+		// получаем переменную интерфейсного типа UsersClient,
+		// через которую будем отправлять сообщения
+		c := pb.NewUserServiceClient(conn)
+
+		res, err := pkg.GetFullList(c, &pb.GetFullListRequest{})
+		if err != nil {
+			log.Println("can not get full list.")
+			return
+		}
+
+		//как вывести сюда список из пг
+		for _, v := range res.Data {
+			fmt.Println(v)
+		}
+	}}
+
+func init() {
+	rootCmd.AddCommand(getFullListCmd)
+}
